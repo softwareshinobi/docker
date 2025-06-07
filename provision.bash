c506978c992a6c4e7fcec84589ca082b92705649
Software Shinobi <troy@softwareshinobi.com>
2025-06-07 16:35:41 -0400

    automated terminal push

--- provision.bash
+++ provision.bash
@@ -15,57 +15,25 @@ echo "## routine / provision-ubuntu-basic / starting"
 echo "## "
 echo
 
-##
+apt-get update;
 
-##apt-get update
+apt -y install ca-certificates curl;
 
-##
+install -m 0755 -d /etc/apt/keyrings
 
-#apt-get upgrade -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 
-#apt-get dist-upgrade -y
-
-##
-
-#apt-get install -y net-tools
-
-#apt-get install -y vim
-
-##
-##
-
-#set -e;
-
-#set -x;
-
-## updating apt
-
-sudo apt-get update;
-
-##
-
-sudo apt -y install ca-certificates curl;
-
-### APT package dependency setup
-
-sudo install -m 0755 -d /etc/apt/keyrings
-
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
+apt update
 
-## docker engine install
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo docker run hello-world
+docker run hello-world
 
 echo "finished provision..."
